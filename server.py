f88a20c7afe29a639abfd26267b9c5f05b16ef0b
SuGallardoY <101368261+SuGallardoY@users.noreply.github.com>
2022-10-11 10:21:36 -0300

    Add files via upload

---
+++ server.py
@@ -0,0 +1,13 @@
+from flask_app import app 
+
+#importar controlador
+
+from flask_app.controllers import users_controller, recipes_controller
+
+
+
+
+
+
+if __name__=="__main__":
+    app.run(debug=True)
